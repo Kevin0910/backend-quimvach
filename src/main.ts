72da60fea180b2fbe85364c489077b646f645d3a
Kevin Gonzalo Cruz Martinez <kevingonzalocruzmartinez@gmail.com>
2025-05-15 22:37:15 -0600

    Se modifico el main

--- src/main.ts
+++ src/main.ts
@@ -6,14 +6,14 @@ import serveStatic from 'serve-static';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['https://forestgreen-rhinoceros-223201.hostingersite.com'],
+    origin: ['http://localhost:4200', 'http://192.168.1.38:4200'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   });
 
   app.use('/uploads/pdfs', serveStatic(join(__dirname, '..', 'uploads', 'pdfs')));
 
 
-  await app.listen(process.env.PORT || 5432);
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
 
 }
 bootstrap();
