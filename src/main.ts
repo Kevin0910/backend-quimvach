dc1f80b69103fe963f2cea216b7a320b1c627227
Kevin Gonzalo Cruz Martinez <kevingonzalocruzmartinez@gmail.com>
2025-05-15 22:22:37 -0600

    Se modifico el main

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
   app.use('/uploads/pdfs', serveStatic(join(__dirname, '..', 'uploads', 'pdfs')));
 
 
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 5432);
 
 }
 bootstrap();
